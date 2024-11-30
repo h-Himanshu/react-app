5a9f2eeb9000b9e95403b798bb03909dd0d82d89
h-Himanshu <chandrahimanshu171@gmail.com>
2024-11-30 23:08:03 +0545

    middle grid color prop set

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,17 @@ import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Layout from './components/Layout'
 import Footer from './components/Footer'
+import Gallary from './components/Gallary'
 function App() {
   const router = createBrowserRouter([
     {
       path : "/",
-      element:<><Navbar/><Layout value = {<Home/>}/><Footer/></>
+      element:<><Navbar/><Layout value = {<Home/>} color = {"violet"}/><Footer/></>
     },
-    // {
-    //   path : "/gallary",
-    //   element:<><Navbar/><Login/></>
-    // },
+    {
+      path : "/gallary",
+      element:<><Navbar/><Layout value = {<Gallary/>} color = {"#bbb"}/><Footer/></>
+    },
     // {
     //   path : "/about",
     //   element:<><Navbar/><About/></>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Footer from './Footer'
 import Form from './Form'
-const Layout = ({value}) => {
+const Layout = (props) => {
   return (
     <>
     <div className="layout">
         <div className="left"></div>
-        <div className="middle">{value}</div>
+        <div className="middle" style={{backgroundColor :props.color}}>{props.value}</div>
         <div className="right"><Form/></div>
     </div>
     
